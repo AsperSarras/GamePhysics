9b1bdeabf39fc569674183a081e5bd8b836ae66a
Asper Sarras <Cristobalsarras@gmail.com>
2021-11-08 14:46:39 -0300

    Lab6

---
+++ Assets/FizziksSys.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FizziksSys : MonoBehaviour
+{
+    public Vector3 gravity = new Vector3(0, -9.81f, 0);
+    public List<TestScript> fizziksObjects = new List<TestScript>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i =0; i < fizziksObjects.Count;i++)
+        {
+            fizziksObjects[i].velocity += gravity * fizziksObjects[i].gravityScale * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/TestScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public float mass = 1.0f;
+    public Vector3 velocity = Vector3.zero;
+    //public FizziksSys fizzikSys;
+    public float gravityScale= 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Hello World! " + gameObject.name + " !");
+        //fizzikSys = FindObjectOfType<FizziksSys>();
+        //fizzikSys.fizziksObjects.Add(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = transform.position + new Vector3(0,Mathf.Sin(Time.time),0);
+        transform.position = transform.position + velocity * Time.deltaTime;
+        //velocity += fizzikSys.gravity * gravityScale * Time.deltaTime;
+    }
+}
